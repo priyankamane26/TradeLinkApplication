14c4491c662137b274d9356e3ad203d0ea328889
sanketh <sbatchir@stevenes.edu>
2016-11-26 16:54:13 -0500

    app.js setup and created an error page.

---
+++ TradeLink/routes/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by sanketh on 11/26/2016.
+ */
+const users = require("./users");
+const data = require("../data");
+const usersData = data.users;
+const path = require('path');
+
+
+const constructorMethod = (app) => {
+    app.use("/users", users);
+
+    app.get("/", function (request, response) {
+        response.render("user/loginform");
+    });
+
+    app.use("*", (req, res) => {
+        let route = path.resolve(`static/errorPage.html`);
+        res.status(404).sendFile(route);
+    })
+};
+
+module.exports = constructorMethod;
