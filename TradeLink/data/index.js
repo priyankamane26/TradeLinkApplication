3197c03c9fac9a1a058d5f9462ecfbc68a6d4695
cbruno10 <codybruno10@gmail.com>
2016-11-27 22:24:49 -0500

    Remove deprecated TradeLink directory

--- TradeLink/data/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by sanketh on 11/26/2016.
- */
-const users = require("./users");
-module.exports = {
-    users: users
-};
--- TradeLink/data/users.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by sanketh on 11/26/2016.
- */
-const mongoCollections = require("../config/mongoCollections");
-const users = mongoCollections.users;
