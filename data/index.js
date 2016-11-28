393649f547132f64b8d20f5f42d9098f240898dd
cbruno10 <codybruno10@gmail.com>
2016-11-27 22:24:27 -0500

    Move TradeLink files to top level

--- data/index.js
+++ data/index.js
@@ -1,3 +1,7 @@
 /**
  * Created by sanketh on 11/26/2016.
  */
+const users = require("./users");
+module.exports = {
+    users: users
+};
---
+++ data/users.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sanketh on 11/26/2016.
+ */
+const mongoCollections = require("../config/mongoCollections");
+const users = mongoCollections.users;
